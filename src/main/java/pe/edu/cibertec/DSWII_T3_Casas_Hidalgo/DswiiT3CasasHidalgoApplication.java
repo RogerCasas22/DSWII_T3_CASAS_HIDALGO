f5d12fcda09cf6dabf6ced43615241ff9647dcc7
RogerCasas22 <roger_1997_xd@hotmail.com>
2024-07-02 19:17:39 -0500

    conexion a bd

---
+++ src/main/java/pe/edu/cibertec/DSWII_T3_Casas_Hidalgo/DswiiT3CasasHidalgoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_T3_Casas_Hidalgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiT3CasasHidalgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiT3CasasHidalgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DSWII_T3_Casas_Hidalgo/DswiiT3CasasHidalgoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_T3_Casas_Hidalgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiT3CasasHidalgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
